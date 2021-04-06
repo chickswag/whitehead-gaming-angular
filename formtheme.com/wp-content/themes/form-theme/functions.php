c5b75d37a57427234c88bbbdcf21819ba0f98b32
nomsa <nomsam@smartz-solutions.com>
2021-04-06 22:24:30 +0200

    delete repo

--- formtheme.com/wp-content/themes/form-theme/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * WordPress
- */
-require_once ( get_template_directory() . '/inc/wp-add-theme-support.php' );
-require_once ( get_template_directory() . '/inc/wp-enqueue-styles.php' );
-
